c29025db3cab7c9cd30f21b6bb84577052e0c798
Sarah Von Ahn <Sarah@dhcp-18-111-118-190.dyn.MIT.EDU>
2017-10-24 17:14:31 -0400

    Seems to make all connections for adjacent districts in Wyoming, will need to find some way to actually test this

--- WY_experiment/WY_case/adj_graph_WY.py
+++ WY_experiment/WY_case/adj_graph_WY.py
@@ -13,9 +13,34 @@ import matplotlib as plt
 import numpy as np
 import pysal as ps
 import random as rdm
+import networkx as nx
 from pysal.contrib.viz import mapping as maps
 from pylab import *
 from matplotlib.collections import LineCollection
+
+
+def distance(p0, p1):
+    """
+    :param p0: first point
+    :param p1: second point
+    :return: distance between first point and second point
+    """
+    return math.sqrt((p0[0] - p1[0])**2 + (p0[1] - p1[1])**2)
+
+
+def share_vertex(the_first_shape, the_second_shape, shape_vertices_dict):
+    """"
+    :param the_first_shape: a shape
+    :param the_second_shape: another shape
+    :param shape_vertices_dict: dictionary that maps shape to all of the vertices of that shape
+    :return: True if shapes share at least one vertex, False otherwise
+    """
+    for the_first_vertex in shape_vertices_dict[the_first_shape]:
+        for the_second_vertex in shape_vertices_dict[the_second_shape]:
+            if distance(the_first_vertex, the_second_vertex) < 1 / (10 ** 10):
+                return True
+    return False
+
 
 # INPUTS
 # string for SHP file directory - user should adjust this as necessary
@@ -24,40 +49,40 @@ file_dir = '/Users/Sarah/Documents/School Docs 17-18/MGGG UROP/Adjacency/state-a
 # map out census blocks
 shp = ps.open(file_dir)
 
-# map all vertices to a centroid
-cents = {(round(v[0],9),round(v[1],9)): (round(s.centroid[0],9),round(s.centroid[1],9)) for s in shp for v in s.vertices}
-cents = {v: s.centroid for s in shp for v in s.vertices}
+# empty graph to get adjacency
+G = nx.Graph()
+
+# map all vertices to a shape
+vertex_collections = {}
+for s in shp:
+    vertex_collections[s] = []
+    G.add_node(s)
+    for v in s.vertices:
+        vertex_collections[s].append(v)
+
+# collect lines between centroids of adjacent shapes
+edge_list = []
+for first_shape in vertex_collections.keys():
+    for second_shape in vertex_collections.keys():
+        if first_shape == second_shape:
+            continue
+        if share_vertex(first_shape, second_shape, vertex_collections):
+            edge_list.append((first_shape.centroid, second_shape.centroid))
+            G.add_edge(first_shape, second_shape)
+
+edge_list = LineCollection(edge_list)
 
 # setting up matplot figure
-fig = plt.figure(figsize=(9,9))
+fig = plt.figure(figsize=(9, 9))
 fig.set_facecolor('white')
 base = maps.map_poly_shp(shp)
 base.set_linewidth(0.75)
 base.set_facecolor('none')
 base.set_edgecolor('0.8')
-
-# create a network to obtain the adjacency graph edges
-WY_network = nw.network.Network(file_dir, unique_segs=True, node_sig=None)
-
-# get an edge list and transform to a matplotlib collection
-edge_list = []
-for start, end in WY_network.edges:
-    try:
-        edge_list.append((cents[WY_network.node_coords[start]], cents[WY_network.node_coords[end]]))
-    except:
-        # TODO: here's the line which could be responsible for the missing edges - unsure how to handle
-        print('i am being excepted')
-        # some coordinates don't appear in the map, this is likely due to small computational discrepancies
-        # at the lower digits of the float --> rounding could help but also takes away edges due to inaccuracies
-        continue
-
-edge_list = LineCollection(edge_list)
-
-# maps._add_axes2col(edge_list,shp.bbox) #figuring out if this could be useful
 edge_list.set_linewidth(0.20)
 ax = maps.setup_ax([base, edge_list], [shp.bbox, shp.bbox])
 fig.add_axes(ax)
 show()
 
 # use this line to save your output
-savefig("Wyoming_adjacency_graph.png")
+# savefig("Wyoming_adjacency_graph.png")
