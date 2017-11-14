a3bc768aa9bfa4d1beaaba803960f6f8cd69dbf6
Alejandro Velez <amva13@mit.edu>
2017-11-14 00:44:47 -0500

    generated adjacency lists

--- adjacency_graphs_all/adj_graph_generator.py
+++ adjacency_graphs_all/adj_graph_generator.py
@@ -18,14 +18,14 @@ from pylab import *
 from matplotlib.collections import LineCollection
 import collections
 import glob
-
+import pandas as pd
 
 # INPUTS
 # string for SHP file directory - user should adjust this as necessary
 # using 2010 Census Data
 # file_dir = "/Users/avelez/Documents/MGGG_UROP/state-adjacency-graphs/MA_experiment/MA_case/tract_file/tl_2010_25_tract10.shp"
 # directory where tract files are contained
-tract_directory = "/Users/avelez/Documents/MGGG_UROP/state-adjacency-graphs/MA_experiment/MA_case/tract_file"
+tract_directory = "/Users/avelez/Documents/MGGG_UROP/state-adjacency-graphs/adjacency_graphs_all/MA_case/tract_file"
 
 
 # define the High Performance Containers and Set Operations two-step algorithm for node adjacency
@@ -48,49 +48,81 @@ def twostep(fname):
 			w[neighbor] = w[neighbor] | neighbors
 	return w
 
+def modified_twostep(d):
+	shpFileObject = d
+	# if shpFileObject.type != ps.cg.Polygon:
+	# 	return
+	numPoly = len(shpFileObject)
+
+	vertices = collections.defaultdict(set)
+	for i, s in shpFileObject.items():
+		newvertices = s.vertices[:-1]
+		for v in newvertices:
+			vertices[v].add(i)
+
+	w = collections.defaultdict(set)
+	for neighbors in vertices.values():
+		for neighbor in neighbors:
+			w[neighbor] = w[neighbor] | neighbors
+	return w
+
 # create list of path names to the tract files
 tract_files = glob.glob(tract_directory+'/*.shp')
+dbf_files = glob.glob(tract_directory+'/*.dbf')
+files = zip(tract_files,dbf_files)
 
-for file_dir in tract_files:
+for file_dir,dbf_file in files:
 
 	state = file_dir[-14:-12]
 
 	# map out census blocks
 	shp = ps.open(file_dir)
+	dbf = ps.open(dbf_file)
+	# print(dbf.header)
+	GEOID_LIST = dbf.by_col_array('GEOID10')
+	tract_list = [x for x in shp]
+	GEOID_to_TRACT = {GEOID_LIST[i][0]:tract_list[i] for i in range(len(tract_list))}
 
-	#TEST
-	# print([x for x in shp])
+	graph = modified_twostep(GEOID_to_TRACT)
 
-	# setting up matplot figure
-	fig = plt.figure(figsize=(9,9))
-	fig.set_facecolor('white')
-	base = maps.map_poly_shp(shp)
-	base.set_linewidth(0.75)
-	base.set_facecolor('none')
-	base.set_edgecolor('0.8')
+	adj_list = [[t, tp] for t in graph.keys() for tp in graph[t]]
 
-	# graph contains polygons matched to their neighbors, uses polygon identifiers
-	graph = twostep(shp)
+	df_adj = pd.DataFrame(adj_list)
+	# print("done with state "+state)
+	df_adj.to_csv("adj_lists/adj_list_"+state+".csv",index=False, header = False)
 
-	# obtain the centroids of polygons
-	polygon_centroids = {x:y.centroid for x,y in enumerate(shp)}
+	# UNCOMMENT THESE TO CREATE PLOTS
 
-	# obtain an edge_list
-	edge_list = [(polygon_centroids[poly1], polygon_centroids[poly2]) for poly1,neighbors in graph.items() for poly2 in neighbors]
-	edge_list = LineCollection(edge_list)
+	# # setting up matplot figure
+	# fig = plt.figure(figsize=(9,9))
+	# fig.set_facecolor('white')
+	# base = maps.map_poly_shp(shp)
+	# base.set_linewidth(0.75)
+	# base.set_facecolor('none')
+	# base.set_edgecolor('0.8')
 
-	#maps._add_axes2col(edge_list,shp.bbox) #figuring out if this could be useful
-	edge_list.set_linewidth(0.20)
-	ax = maps.setup_ax([base, edge_list], [shp.bbox, shp.bbox])
-	fig.add_axes(ax)
+	# # graph contains polygons matched to their neighbors, uses polygon identifiers
+	# graph = twostep(shp)
 
-	# use this line to see your output
-	# show()
+	# # obtain the centroids of polygons
+	# polygon_centroids = {x:y.centroid for x,y in enumerate(shp)}
 
-	# use this line to save your output
-	savefig("adjacency_graphs/adjacency_graph_"+state+".png")
+	# # obtain an edge_list
+	# edge_list = [(polygon_centroids[poly1], polygon_centroids[poly2]) for poly1,neighbors in graph.items() for poly2 in neighbors]
+	# edge_list = LineCollection(edge_list)
 
-	plt.close()
+	# #maps._add_axes2col(edge_list,shp.bbox) #figuring out if this could be useful
+	# edge_list.set_linewidth(0.20)
+	# ax = maps.setup_ax([base, edge_list], [shp.bbox, shp.bbox])
+	# fig.add_axes(ax)
+
+	# # use this line to see your output
+	# # show()
+
+	# # use this line to save your output
+	# savefig("adjacency_graphs/adjacency_graph_"+state+".png")
+
+	# plt.close()
 
 
 
