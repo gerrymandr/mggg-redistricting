c5728a8dab25660db3606163041f59de0d7bd34e
Alejandro Velez <amva13@mit.edu>
2017-11-25 23:07:06 -0500

    fixed boundary tract detection and cleaned up repo

--- adjacency_graphs_all/compute_tract_membership_and_overlap_with_districts.py
+++ adjacency_graphs_all/compute_tract_membership_and_overlap_with_districts.py
@@ -28,6 +28,8 @@ threshold = 0.00001 # for determining boundary nodes
 
 # create list of path names to the tract files
 tract_files = glob.glob(tract_directory+'/*.shp')
+dbf_files = glob.glob(tract_directory+'/*.dbf')
+files = zip(tract_files,dbf_files)
 
 # define the High Performance Containers and Set Operations two-step algorithm for node adjacency
 # source: http://conference.scipy.org/proceedings/scipy2014/pdfs/laura.pdf
@@ -39,6 +41,24 @@ def twostep(fname):
 
 	vertices = collections.defaultdict(set)
 	for i, s in enumerate(shpFileObject):
+		newvertices = s.vertices[:-1]
+		for v in newvertices:
+			vertices[v].add(i)
+
+	w = collections.defaultdict(set)
+	for neighbors in vertices.values():
+		for neighbor in neighbors:
+			w[neighbor] = w[neighbor] | neighbors
+	return w
+
+def modified_twostep(d):
+	shpFileObject = d
+	# if shpFileObject.type != ps.cg.Polygon:
+	# 	return
+	numPoly = len(shpFileObject)
+
+	vertices = collections.defaultdict(set)
+	for i, s in shpFileObject.items():
 		newvertices = s.vertices[:-1]
 		for v in newvertices:
 			vertices[v].add(i)
@@ -85,52 +105,66 @@ iterate over the tract files and compute the following
 # number of boundary tracts and number of member tracts
 entries = []
 node_membership = []
-for tract_file in tract_files:
+for tract_file, dbf_file in files:
 	shp = ps.open(tract_file)
-	graph = twostep(shp)
-	tid_to_poly = {x:y for x,y in enumerate(shp)}
+	# tid_to_poly = {x:y for x,y in enumerate(shp)}
 	state = tract_file[-14:-12]
-	# state_tracts = ps.cg.locators.PolygonLocator([poly for poly in shp]) # package bug ..
+
+	dbf = ps.open(dbf_file)
+	# print(dbf.header)
+	GEOID_LIST = dbf.by_col_array('GEOID10')
+	tract_list = [x for x in shp]
+	GEOID_to_TRACT = {GEOID_LIST[i][0]:tract_list[i] for i in range(len(tract_list))}
+
+	# graph = modified_twostep(GEOID_to_TRACT)
+
+	
 	for district in STATE_TO_DISTRICTS[state]:
-		# tracts_to_consider = state_tracts.inside(district.bounding_box) # package bug ..
-		# tracts_to_consider = graph.items()
-		# ct = 0 #placeholder to tract IDs
 		d = sg.asShape(district)
 		dbox = sg.box(*sg.asShape(district).bounds)
 		dboundary = set()
 		dmember = set()
+		chainD = ps.cg.shapes.Chain(district.vertices)
 
-		for tid,tract in tid_to_poly.items():
-			#print(tract)
+		for tid,tract in GEOID_to_TRACT.items():
 			t = sg.box(*tract.bbox)
+			chainT = ps.cg.shapes.Chain(tract.vertices)
 			if t.intersects(dbox):
-				# print("yep1")
-				if d.intersects(t):
-					# print("yep2")
-					area = d.intersection(t).area
-					entries.append([DISTRICT_TO_GEOID[district], tid, area, area/t.area])
-				# ct+=1
-					# if all(d.intersection(sg.box(*tid_to_poly[x].bbox)).area/sg.box(*tid_to_poly[x].bbox).area >0.5 for x in graph[tid]):
+				tr = sg.asShape(tract)
+				if d.intersects(tr):
+					# area = d.intersection(t).area
+					# entries.append([DISTRICT_TO_GEOID[district], tid, area, area/t.area])
+
+					# check if boundaries intersect
+					#they_do = any(sg.asShape(dc).intersects(sg.asShape(tc)) for dc in chainD.segments for tc in chainT.segments)
+					they_do = d.intersection(t.buffer(0.001)).area/t.buffer(0.001).area < 1
+					if they_do:
+						dboundary.update([tid])
+					dmember.update([tid])
+
+					# determine whether the tract is boundary or area
+					# if area/t.area < (1.0-threshold):
+					# 	dboundary.update([tid])
+					#elif check if touch boundary --> boundary
+					#else its an area node
+					
+					# if area/t.area >= (1.0-threshold):
 					# 	dmember.update([tid])
 					# else:
 					# 	dboundary.update([tid])
-					if area/t.area >= (1.0-threshold):
-						dmember.update([tid])
-					else:
-						dboundary.update([tid])
 		node_membership.append([DISTRICT_TO_GEOID[district],len(dboundary),len(dmember)])
 
 
 
-print(entries)
-print(node_membership)
+# print(entries)
+# print(node_membership)
 # print(tract_files[0])
 
 # save results
 
-df_entries = pd.DataFrame(entries)
+# df_entries = pd.DataFrame(entries)
 df_node_membership = pd.DataFrame(node_membership)
 
-df_entries.to_csv("district_to_tracts_overlap_new2.csv", index=False, header = False)
-df_node_membership.to_csv("node_membership_new2.csv", index=False,header=False)
+#df_entries.to_csv("district_to_tracts_overlap_new.csv", index=False, header = False)
+df_node_membership.to_csv("node_membership_new.csv", index=False,header=False)
 
