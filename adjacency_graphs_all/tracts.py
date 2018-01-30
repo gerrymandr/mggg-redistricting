d80610c9d65afd3b6018061d139b6e0a754190c5
Alejandro Velez <amva13@mit.edu>
2018-01-29 20:38:20 -0500

    writing shapefiles

--- adjacency_graphs_all/tracts.py
+++ adjacency_graphs_all/tracts.py
@@ -17,9 +17,10 @@ cd_col = 'GEOID'
 state_col = 'STATEFP'
 begin = -14
 end = -12
+shp_name = "tracts.shp"
 
 # obtain the lists-of-lists
-entries, node_membership = get_district_member_and_boundary_entities(shp_and_dbf_file_dir, cd_dbf_dir, cd_shp_dir, sub_geoid_col, cd_col, state_col, begin, end)
+entries, node_membership = get_district_member_and_boundary_entities(shp_and_dbf_file_dir, cd_dbf_dir, cd_shp_dir, sub_geoid_col, cd_col, state_col, begin, end, shp_name)
 
 df_entries = pd.DataFrame(entries)
 df_node_membership = pd.DataFrame(node_membership)
