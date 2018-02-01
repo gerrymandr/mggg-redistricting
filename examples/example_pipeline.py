39dcd206cbd0a9533d5787cfc13d0900be3f319d
Seth-Rothschild <seth.j.rothschild@gmail.com>
2018-02-01 15:41:29 -0600

    Add example, bring twostep and polymap to init

---
+++ examples/example_pipeline.py
@@ -0,0 +1,8 @@
+from adjacency_graphs.MgggGraph import MgggGraph
+
+shp_dir = 'shapefiles/testershape.shp'
+geoid_column = 'id'
+
+my_graph = MgggGraph(shp_dir, geoid_column)
+import pdb
+pdb.set_trace()
