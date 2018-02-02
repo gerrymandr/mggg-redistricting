2de6fd78d1b46d2c63447e49e667df8d1f24e650
Seth-Rothschild <seth.j.rothschild@gmail.com>
2018-02-02 17:33:15 -0600

    make an example plot

--- examples/example_pipeline.py
+++ examples/example_pipeline.py
@@ -1,11 +1,12 @@
-from adjacency_graphs.algorithms.twostep import TwoStepGraph
-#from adjacency_graphs.plots.mpl import visualize_adjacency_graph
+from adjacency_graphs.algorithms import TwoStepGraph
+from adjacency_graphs.plots.mpl import visualize_adjacency_graph
+from matplotlib.pyplot import savefig
 
-shp_dir = 'tests/shapefiles/testershape.shp'
+shp_dir = '../tests/shapefiles/testershape.shp'
 geoid_column = 'id'
 
 my_graph = TwoStepGraph(shp_dir, geoid_column)
 
 # Line below is broken
-#fig = visualize_adjacency_graph(my_graph)
-# fig.show()
+fig = visualize_adjacency_graph(my_graph)
+savefig('example.png')
