039241563f20d37a80c16289693fc955b9584e82
Seth-Rothschild <seth.j.rothschild@gmail.com>
2018-02-03 15:33:24 -0600

    bugfix in export_graph.py

--- adjacency_graphs/utils/export_graph.py
+++ adjacency_graphs/utils/export_graph.py
@@ -1,11 +1,14 @@
+import pandas as pd
+
+
 def export_graph(mggg_graph, output_path='graph.csv'):
     data_shape = mggg_graph.shape_df
     data_neighbors = mggg_graph.neighbors
     # Adjacency dataframe
-    adjacency_df = pd.DataFrame(columns=['GEOID', 'vertices'])
+    adjacency_df = pd.DataFrame(columns=[mggg_graph.id_column, 'vertices'])
     for item in data_neighbors.items():
-        adjacency_df = adjacency_df.append(pd.DataFrame(columns=['GEOID', 'vertices'], 
+        adjacency_df = adjacency_df.append(pd.DataFrame(columns=[mggg_graph.id_column, 'vertices'],
                                                         data=[[item[0], item[1]]]))
     # Adding adjacency data to dataframe.
-    data = data_shape.merge(adjacency_df, on='GEOID', how='outer')
+    data = data_shape.merge(adjacency_df, on=mggg_graph.id_column, how='outer')
     data.to_csv(output_path)
