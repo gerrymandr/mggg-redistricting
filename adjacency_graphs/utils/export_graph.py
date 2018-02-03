7b6ba1e9dc7383a4a673a64452c71053fc4fdf3a
Seth-Rothschild <seth.j.rothschild@gmail.com>
2018-02-03 15:36:45 -0600

    update `export_graph`

--- adjacency_graphs/utils/export_graph.py
+++ adjacency_graphs/utils/export_graph.py
@@ -3,7 +3,9 @@ import pandas as pd
 
 def export_graph(mggg_graph, output_path='graph.csv'):
     data_shape = mggg_graph.shape_df
+    data_shape = data_shape.drop('geometry', 1)
     data_neighbors = mggg_graph.neighbors
+
     # Adjacency dataframe
     adjacency_df = pd.DataFrame(columns=[mggg_graph.id_column, 'vertices'])
     for item in data_neighbors.items():
