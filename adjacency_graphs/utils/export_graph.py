a65891d1301cd9bd47907b611aa45c4d9ae0b54f
nelabdiel <nel.abdiel@gmail.com>
2018-02-03 16:29:09 -0500

    dataframe to csv now contains information about neighbors

--- adjacency_graphs/utils/export_graph.py
+++ adjacency_graphs/utils/export_graph.py
@@ -1,3 +1,11 @@
 def export_graph(mggg_graph, output_path='graph.csv'):
-    data = mggg_graph.shape_df
+    data_shape = mggg_graph.shape_df
+    data_neighbors = mggg_graph.neighbors
+    # Adjacency dataframe
+    adjacency_df = pd.DataFrame(columns=['GEOID', 'vertices'])
+    for item in data_neighbors.items():
+        adjacency_df = adjacency_df.append(pd.DataFrame(columns=['GEOID', 'vertices'], 
+                                                        data=[[item[0], item[1]]]))
+    # Adding adjacency data to dataframe.
+    data = data_shape.merge(adjacency_df, on='GEOID', how='outer')
     data.to_csv(output_path)
